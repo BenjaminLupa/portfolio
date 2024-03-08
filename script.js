1b501f48bb39da45509457ff89b9ec219e278573
Benjamin Lupa <147424978+BenjaminLupa@users.noreply.github.com>
2024-03-08 09:10:43 +0100

    feat: added mediaScreen

--- script.js
+++ script.js
@@ -61,3 +61,37 @@ circles.forEach((element) => {
 //mix it up PORTFOLIO section
 
 var mixer = mixitup(".portfolio-gallery");
+
+//active menu //////////////////////////
+let menuLi = document.querySelectorAll("header ul li a");
+let section = document.querySelectorAll("section");
+
+function activeMenu() {
+  let len = section.length;
+  while (--len && window.scrollY + 97 < section[len].offsetTop) {}
+  menuLi.forEach((sec) => sec.classList.remove("active"));
+  menuLi[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll", activeMenu);
+
+// sticky navbar ////////////////////
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 50);
+});
+
+// toggle icon navbar ////////////////////
+let menuIcon = document.querySelector("#menu-icon");
+let navList = document.querySelector(".navlist");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navList.classList.toggle("open");
+};
+
+window.onscroll = () => {
+  menuIcon.classList.remove("bx-x");
+  navList.classList.remove("open");
+};
